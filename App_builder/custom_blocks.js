e742aa111556bb68fa277142a2d75e37ffba1fff
Aidan Lewis-Grenz <70214773+AidanFLG@users.noreply.github.com>
2023-10-09 15:18:35 -0700

    Update custom_blocks.js

--- App_builder/custom_blocks.js
+++ App_builder/custom_blocks.js
@@ -15,7 +15,7 @@ Blockly.Blocks['var_body'] = {
   init: function() {
     this.appendValueInput("BODY_NAME")
         .appendField("Create var body with name:");
-        .appendField(new Blockly.FieldTextInput("Enter var"), "NAME");
+        .appendField(new Blockly.FieldTextInput("Enter name"), "NAME");
     this.appendStatementInput("BODY_CONTENT")
         .setCheck(["Variable", "var_body"])
         .appendField("Body Content");
@@ -31,8 +31,29 @@ Blockly.Blocks['var_body'] = {
 Blockly.Blocks['variable'] = {
   init: function() {
     this.appendValueInput("VAR_NAME")
-        .setCheck(["String", "Variable"])
-        .appendField("Create variable named:");
+        .appendField("variable named:");
+        .appendField(new Blockly.FieldTextInput("Enter name"), "NAME");
+    this.appendValueInput("VAR_VALUE")
+        .setCheck(null)
+        .appendField("Value:");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+    this.setTooltip("Create a variable");
+    this.setHelpUrl("");
+  }
+};
+
+// Create a variable block
+Blockly.Blocks['state_variable'] = {
+  init: function() {
+    this.appendValueInput("VAR_STATE")
+        .appendField("Public or Private:");
+        .appendField(new Blockly.FieldTextInput("public or private"), "STATE");
+    this.appendValueInput("VAR_NAME")
+        .appendField("state variable named:");
+        .appendField(new Blockly.FieldTextInput("Enter name"), "NAME");
     this.appendValueInput("VAR_VALUE")
         .setCheck(null)
         .appendField("Value:");
