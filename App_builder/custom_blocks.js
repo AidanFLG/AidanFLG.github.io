44cb43bde47e7e5c77e7cddedc917ae99c7f1f0b
Tony Bautista <jubautista@chapman.edu>
2023-10-13 00:02:02 -0700

    Update custom_blocks.js

--- App_builder/custom_blocks.js
+++ App_builder/custom_blocks.js
@@ -1,21 +1,21 @@
-// Import SwiftUI and create ContentView block
-Blockly.Blocks['import_swiftui'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Import SwiftUI")
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
+// // Import SwiftUI and create ContentView block
+// Blockly.Blocks['import_swiftui'] = {
+//   init: function() {
+//     this.appendDummyInput()
+//         .appendField("Import SwiftUI")
+//     this.setPreviousStatement(true, null);
+//     this.setNextStatement(true, null);
+//     this.setColour(230);
+//     this.setTooltip("");
+//     this.setHelpUrl("");
+//   }
+// };
 
-// Generates import in swift
-Blockly.JavaScript['import_swiftui'] = function(block) {
-  var code = "import SwiftUI\n";
-  return code;
-};
+// // Generates import in swift
+// Blockly.JavaScript['import_swiftui'] = function(block) {
+//   var code = "import SwiftUI\n";
+//   return code;
+// };
 
 // Import SwiftUI and create ContentView block
 Blockly.Blocks['import'] = {
